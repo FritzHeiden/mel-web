4479e621512a8e21160e6283cb4521b8f24c1954
Fritz Heiden <fritz.heiden@mailbox.org>
2018-04-22 13:21:35 +0200

    made download service use event emitter class

--- src/services/download-service.js
+++ src/services/download-service.js
@@ -1,11 +1,15 @@
 import { Artist } from 'mel-core'
 import JSZip from 'jszip'
+import EventEmitter from '../tools/event-emitter'
+
+const DOWNLOAD_LIST_CHANGE = 'download_list_change'
 
 class DownloadService {
   constructor (melHttpService) {
     this._artists = []
     this._listeners = []
     this._melHttpService = melHttpService
+    this._eventEmitter = new EventEmitter()
   }
 
   get artists () {
@@ -13,38 +17,34 @@ class DownloadService {
   }
 
   addOnDownloadListChangedListener (listener) {
-    this._listeners.push(listener)
+    this._eventEmitter.on(DOWNLOAD_LIST_CHANGE, listener)
   }
 
   addArtist (newArtist) {
-    let artist = this._artists.find(artist => (artist.id = newArtist.id))
+    let artist = this._artists.find(artist => artist.id === newArtist.id)
     if (!artist) {
       this._artists.push(newArtist)
-      this._listeners.forEach(listener => listener(this._artists))
     } else {
       newArtist.album.forEach(album => this.addAlbum(album))
     }
+    this._eventEmitter.invokeAll(DOWNLOAD_LIST_CHANGE, this._artists)
   }
 
   addAlbum (newAlbum) {
     let artist = this._artists.find(artist => artist.id === newAlbum.artist.id)
     if (!artist) {
-      let artist = new Artist(newAlbum.artist.id, newAlbum.artist.name)
+      let artist = newAlbum.artist
       artist.addAlbum(newAlbum)
       this._artists.push(artist)
-      this._listeners.forEach(listener => listener(this._artists))
     } else {
       let album = artist.albums.find(album => album.id === newAlbum.id)
       if (!album) {
         artist.addAlbum(newAlbum)
-        this._listeners.forEach(listener => listener(this._artists))
       } else {
-        newAlbum.tracks.forEach(track => {
-          track.album = newAlbum
-          this.addTrack(track)
-        })
+        newAlbum.tracks.forEach(track => this.addTrack(track))
       }
     }
+    this._eventEmitter.invokeAll(DOWNLOAD_LIST_CHANGE, this._artists)
   }
 
   addTrack (newTrack) {
@@ -54,25 +54,23 @@ class DownloadService {
     if (!artist) {
       let album = newTrack.album
       album.addTrack(newTrack)
-      let artist = newTrack.album.artist
+      let artist = album.artist
       artist.addAlbums(album)
       this._artists.push(artist)
-      this._listeners.forEach(listener => listener(this._artists))
     } else {
       let album = artist.albums.find(album => album.id === newTrack.album.id)
       if (!album) {
         let album = newTrack.album
         album.addTrack(newTrack)
         artist.addAlbum(album)
-        this._listeners.forEach(listener => listener(this._artists))
       } else {
         let track = album.tracks.find(track => track.id === newTrack.id)
         if (!track) {
           album.addTrack(newTrack)
-          this._listeners.forEach(listener => listener(this._artists))
         }
       }
     }
+    this._eventEmitter.invokeAll(DOWNLOAD_LIST_CHANGE, this._artists)
   }
 
   containsAlbum (needleAlbum) {
@@ -98,7 +96,7 @@ class DownloadService {
 
   deleteList () {
     this._artists = []
-    this._listeners.forEach(listener => listener(this._artists))
+    this._eventEmitter.invokeAll(DOWNLOAD_LIST_CHANGE, this._artists)
   }
 
   async startDownload () {
