72d9acb788eb5aaa712a7bcd4847052e49cb6997
Fritz Heiden <fritz.heiden@mailbox.org>
2018-10-28 15:47:11 +0100

    removed mel-web dependency from webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,6 @@ const fs = require('fs')
 
 const DIST_DIR = path.join(__dirname, 'dist')
 const SRC_DIR = path.join(__dirname, 'src')
-const MODULES_DIR = path.join(__dirname, 'node_modules')
-const MEL_WEB_DIR = fs.realpathSync(
-  path.join(MODULES_DIR, 'mel-core/dist/mel-web')
-)
-
 let nodeModules = {}
 fs
   .readdirSync(path.resolve(__dirname, 'node_modules'))
@@ -30,16 +25,6 @@ module.exports = {
         test: /\.js$/,
         loader: 'babel-loader',
         exclude: /node_modules/
-      },
-      {
-        test: /.+/,
-        loader: 'file-loader',
-        include: MEL_WEB_DIR,
-        options: {
-          name: '[path][name].[ext]',
-          outputPath: 'mel-web',
-          context: MEL_WEB_DIR
-        }
       }
     ]
   },
